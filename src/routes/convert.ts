383c6cfe4440b860167d3207e68420fc6870f3da
Hung Ngo <hung.ngovinh97@gmail.com>
2020-07-14 17:31:02 -0700

    Added nodemon for dev, refactored the main app

--- src/routes/convert.ts
+++ src/routes/convert.ts
@@ -3,25 +3,24 @@ import SVGtoPDF from 'svg-to-pdfkit';
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
 
-
-let router = express.Router();
+const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
-    if (!req.body.svg) {
-        return res.status(400).end('Must provide svg for rendering');
-    }
-    let { svg } = req.body;
-    console.log('Svg received', svg);
+  if (!req.body.svg) {
+    return res.status(400).end('Must provide svg for rendering');
+  }
+  const { svg } = req.body;
+  console.log('Svg received', svg);
 
-    let doc = new PDFDocument();
-    let stream = fs.createWriteStream('file.pdf');
+  const doc = new PDFDocument();
+  const stream = fs.createWriteStream('file.pdf');
 
-    SVGtoPDF(doc, svg, 0, 0);
-    stream.on('finish', () => {
-        console.log(fs.readFileSync('file.pdf'));
-    })
+  SVGtoPDF(doc, svg, 0, 0);
+  stream.on('finish', () => {
+    console.log(fs.readFileSync('file.pdf'));
+  });
 
-    doc.pipe(stream);
-    doc.end();
-    console.log('Finish writing pdf file');
+  doc.pipe(stream);
+  doc.end();
+  console.log('Finish writing pdf file');
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,101 +5,101 @@ import vegaUrlParser from 'vega-schema-url-parser';
 import { URL } from 'url';
 import { ALLOWED_URLS, VEGA_DATA_BASE_URL } from '../constants';
 
-let router = express.Router();
+const router = express.Router();
 // define a route handler for the default home page
 router.get('/', (req: Request, res: Response) => {
-    res
-        .status(200)
-        .send(
-            '<html><body><p>Vega render service. Learn more at <a href="https://github.com/vega/vega-render-service">github.com/vega/vega-render-service</a>.</p></body></html>',
-        );
+  res
+    .status(200)
+    .send(
+      '<html><body><p>Vega render service. Learn more at <a href="https://github.com/vega/vega-render-service">github.com/vega/vega-render-service</a>.</p></body></html>',
+    );
 });
 
 router.post('/', async (req: Request, res: Response) => {
-    const contentType = req.header('Accept') ?? 'pdf';
-    if (!req.body.spec) {
-        return res.status(400).end('Must provide Vega spec for render service');
+  const contentType = req.header('Accept') ?? 'pdf';
+  if (!req.body.spec) {
+    return res.status(400).end('Must provide Vega spec for render service');
+  }
+  let { spec } = req.body;
+  const baseURL = req.body.baseURL || VEGA_DATA_BASE_URL;
+  const { library } = vegaUrlParser(spec.$schema);
+
+  switch (library) {
+    case 'vega':
+      break;
+    case 'vega-lite':
+      spec = compile(spec).spec;
+      break;
+    default:
+      return res
+        .status(400)
+        .end('Invalid Schema, should be Vega or Vega-Lite.');
+  }
+
+  const loader = vega.loader({ mode: 'http' });
+  const originalLoad = loader.load.bind(loader);
+  const originalHttp = loader.http.bind(loader);
+
+  loader.http = async (uri: string, options: any): Promise<string> => {
+    const parsedUri = new URL(uri);
+    if (
+      ALLOWED_URLS.every(
+        (allowedUrl) => !parsedUri.hostname.includes(allowedUrl),
+      )
+    ) {
+      res.status(400).send('External URI not allowed on this API');
+      throw new Error('External data url not allowed');
     }
-    let { spec } = req.body;
-    const baseURL = req.body.baseURL || VEGA_DATA_BASE_URL;
-    const { library } = vegaUrlParser(spec.$schema);
+    return originalHttp(uri, options);
+  };
 
-    switch (library) {
-        case 'vega':
-            break;
-        case 'vega-lite':
-            spec = compile(spec).spec;
-            break;
-        default:
-            return res
-                .status(400)
-                .end('Invalid Schema, should be Vega or Vega-Lite.');
+  loader.load = async (url, options) => {
+    try {
+      if (options) {
+        return await originalLoad(url, {
+          ...options,
+          ...{ baseURL },
+        });
+      }
+      return await originalLoad(url, { baseURL });
+    } catch {
+      return await originalLoad(url, options);
     }
+  };
 
-    const loader = vega.loader({ mode: 'http' });
-    const originalLoad = loader.load.bind(loader);
-    const originalHttp = loader.http.bind(loader);
-
-    loader.http = async (uri: string, options: any): Promise<string> => {
-        const parsedUri = new URL(uri);
-        if (
-            ALLOWED_URLS.every(
-                (allowedUrl) => !parsedUri.hostname.includes(allowedUrl),
-            )
-        ) {
-            res.status(400).send('External URI not allowed on this API');
-            throw new Error('External data url not allowed');
-        }
-        return originalHttp(uri, options);
-    };
-
-    loader.load = async (url, options) => {
-        try {
-            if (options) {
-                return await originalLoad(url, {
-                    ...options,
-                    ...{ baseURL },
-                });
-            }
-            return await originalLoad(url, { baseURL });
-        } catch {
-            return await originalLoad(url, options);
-        }
-    };
-
-    const view = new vega.View(vega.parse(spec), {
-        renderer: 'none',
-        loader: loader,
-    });
-    view.finalize();
-    switch (contentType) {
-        case 'application/pdf':
-            const pdf = await view.toCanvas(undefined, {
-                type: 'pdf',
-                context: { textDrawingMode: 'glyph' },
-            });
-            const encodedPdf = (pdf as any).toBuffer();
-            if (res.headersSent) {
-                return;
-            }
-            res.status(200).send(encodedPdf);
-            break;
-        case 'image/png':
-            const png = await view.toCanvas();
-            const encodedPng = (png as any).toBuffer();
-            if (res.headersSent) {
-                return;
-            }
-            res.status(200).send(encodedPng);
-        case 'image/vegaSvg':
-        default:
-            const svg = await view.toSVG();
-            if (res.headersSent) {
-                return;
-            }
-            res.status(200).send(svg);
-            break;
-    }
+  const view = new vega.View(vega.parse(spec), {
+    renderer: 'none',
+    loader: loader,
+  });
+  view.finalize();
+  switch (contentType) {
+    case 'application/pdf':
+      const pdf = await view.toCanvas(undefined, {
+        type: 'pdf',
+        context: { textDrawingMode: 'glyph' },
+      });
+      const encodedPdf = (pdf as any).toBuffer();
+      if (res.headersSent) {
+        return;
+      }
+      res.status(200).send(encodedPdf);
+      break;
+    case 'image/png':
+      const png = await view.toCanvas();
+      const encodedPng = (png as any).toBuffer();
+      if (res.headersSent) {
+        return;
+      }
+      res.status(200).send(encodedPng);
+    case 'image/vegaSvg':
+    default:
+      const svg = await view.toSVG();
+      if (res.headersSent) {
+        return;
+      }
+      res.status(200).send(svg);
+      break;
+  }
 });
 
 export { router };
