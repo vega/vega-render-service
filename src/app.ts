f634578741b55ed452a07c08c799b7f8feb2e816
P42 <72252241+p42-ai[bot]@users.noreply.github.com>
2020-12-07 21:46:25 +0000

    Use Template Literals
    **Template literals are string templates that can include variables and expressions.** They can replace string concatenation and are often easier to read. Example: `` `Hello, ${name}!` ``
    
    This refactoring supports converting the first line of concatenated strings into template literals or shortened strings:
    * concatenated string literals will be combined, e.g. `"a" + "b"` will become `"ab"`
    * concatenated expressions and variables will be embedded in templates, e.g. `"a" + x + "b"` will become `` `a${x}b` ``
    * unnecesary escaping of quotes is removed when string are combined or transformed into template literals

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ import { compile } from 'vega-lite';
 import vegaUrlParser from 'vega-schema-url-parser';
 import { ALLOWED_URLS, VEGA_DATA_BASE_URL } from './constants';
 
-if (fs.existsSync(__dirname + '/public/fonts/Roboto/Roboto.ttf')) {
-  registerFont(__dirname + '/public/fonts/Roboto/Roboto.ttf', {
+if (fs.existsSync(`${__dirname}/public/fonts/Roboto/Roboto.ttf`)) {
+  registerFont(`${__dirname}/public/fonts/Roboto/Roboto.ttf`, {
     family: 'Roboto',
   });
 }
@@ -20,7 +20,7 @@ const app: Express = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
