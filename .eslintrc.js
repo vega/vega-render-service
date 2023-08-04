2b937a8014dc569028403657bba2c6018d3a0407
Dominik Moritz <domoritz@gmail.com>
2023-08-03 23:52:29 -0400

    feat: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,7 @@ module.exports = {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+  }
 };
